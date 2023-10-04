c0e8c6444522c50c8a61d0f6d11f1da1bdbd390e
Степан Молчанов <stepanmol4anoff@yandex.ru>
2023-10-05 00:37:23 +0300

    Modified register page, added basics of JWT auth

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,33 +1,60 @@
+from datetime import timedelta
+from typing import Annotated
 from fastapi import (
-    Body, Depends, status
+    Depends, status
 )
 from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
 from . import server
 from .db_initializer import get_db
-from .models.users import User
 from .schemas.users import (
-    CreateUserSchema, UserSchema
+    UserRegisterSchema
 )
+from .schemas.tokens import Token
 from .utils import (
-    create_user, is_password_correct
+    authenticate_user, create_access_token, create_user, is_password_correct
 )
 
 __all__ = (
     'register',
+    'login_for_access_token'
 )
+
+TOKEN_EXPIRE_MINUTES = 180
 
 
 @server.post('/register')
-def register(
-        payload: CreateUserSchema = Body(),
-        session: Session = Depends(get_db)
-):
-    password = payload.password_hash
-    if not is_password_correct(password):
+def register(form_data: Annotated[UserRegisterSchema, Depends()], session=get_db()):
+    exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        headers={'WWW_Authenticate': 'Bearer'}
+    )
+    if form_data.password != form_data.password_again:
+        exception.detail = 'Passwords does not match'
+        raise exception
+    user = create_user(session, form_data.username, form_data.password)
+    if not user:
+        exception.detail = 'Bad password'
+        raise exception
+    access_token_expires = timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={'sub': user.username}, expires_delta=access_token_expires
+    )
+    return JSONResponse({'access-token': access_token, 'token-type': 'bearer'})
+
+
+@server.post('/token', response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], session=get_db()):
+    user = authenticate_user(session, form_data.username, form_data.password)
+    if not user:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Incorrect username or password',
+            headers={'WWW-Authenticate': 'Bearer'}
         )
-    payload.password_hash = User.get_password_hash(password)
-    return JSONResponse(create_user(session, payload).to_dict())
+    access_token_expires = timedelta(minutes=TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={'sub': user.username}, expires_delta=access_token_expires
+    )
+    return JSONResponse({'access_token': access_token, 'token-type': 'bearer'})
